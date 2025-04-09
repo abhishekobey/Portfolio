13b330faafd038df77f196eccb0d537ae17dda1b
abhishek <abhishekobey1122@gmail.com>
2025-04-09 20:26:32 +0530

    :art: little UI adjustment

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div id="contact" className={"contact"}>
-            <h2>Let's Connect</h2>
+            <h1 style={{margin: "100px"}}>Let's Connect</h1>
             <iframe src="https://surveyheart.com/form/67f67aa7fa6fac248afa1e64">Loading...</iframe>
             <div className={"hider"}/>
         </div>
