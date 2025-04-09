95d03f4d682f2dd648d97aec6bac5c2bcd40e03c
abhishek <abhishekobey1122@gmail.com>
2025-04-09 21:03:31 +0530

    :iphone: responsive nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,36 @@
-import React from "react"
-import "../styles/NavBar.scss"
+import React, {useState} from "react";
+import "../styles/NavBar.scss";
 
 const NavBar = () => {
+    const [isOpenMenu, setIsOpenMenu] = useState(false);
+
     const handleScroll = (id) => {
         const element = document.getElementById(id);
         if (element) {
-            element.scrollIntoView({ behavior: "smooth" });
+            element.scrollIntoView({behavior: "smooth"});
+        }
+        if (isOpenMenu) {
+            setIsOpenMenu(false);
         }
     };
 
+    const toggleMobileMenu = () => {
+        setIsOpenMenu(!isOpenMenu);
+    };
+
     return (
-        <div className={"navbar"}>
-            <span onClick={() => handleScroll("home")}>Home</span>
-            <span onClick={() => handleScroll("about")}>About</span>
-            <span onClick={() => handleScroll("experience")}>Experience</span>
-            <span onClick={() => handleScroll("contact")}>Contact</span>
+        <div className="navbar" style={!isOpenMenu ? {height: "auto"} : null}>
+            <div className="navbar__hamburger">
+                <a style={{textAlign: "end"}} onClick={toggleMobileMenu}>
+                    {isOpenMenu ? "✖" : "☰"}
+                </a>
+            </div>
+            <div className={isOpenMenu ? "navbar__hamburger" : "navbar-links"}>
+                <span onClick={() => handleScroll("home")}>Home</span>
+                <span onClick={() => handleScroll("about")}>About</span>
+                <span onClick={() => handleScroll("experience")}>Experience</span>
+                <span onClick={() => handleScroll("contact")}>Contact</span>
+            </div>
         </div>
     );
 };
