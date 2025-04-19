79ec8c4b2a4d347364d5ddf28d601e165e5e79e3
abhishek <abhishekobey1122@gmail.com>
2025-04-19 14:51:05 +0530

    :iphone: responsive contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 
 const Contact = () => {
 
-    const maxHeightPercent = 6.5;
+    const maxHeightPercentPC = 6.5;
+    const maxHeightPercentMobile = 1;
 
     window.addEventListener('scroll', () => {
-        const hider = document.getElementById('hider');
+        const hider = document.getElementById('pc-hider');
         const scrollTop = window.scrollY;
         const docHeight = document.body.scrollHeight;
         const winHeight = window.innerHeight;
         const scrollBottom = docHeight - (scrollTop + winHeight); // Distance from bottom
 
-        const maxScrollDistance = (maxHeightPercent / 100) * docHeight; // max height in px
+        const maxScrollDistance = (maxHeightPercentPC / 100) * docHeight; // max height in px
+        let heightPx = maxScrollDistance - scrollBottom;
+
+        heightPx = Math.max(0, Math.min(maxScrollDistance, heightPx)); // clamp between 0 and max
+
+        hider.style.height = heightPx + 'px';
+    });
+
+    window.addEventListener('scroll', () => {
+        const hider = document.getElementById('mobile-hider');
+        const scrollTop = window.scrollY;
+        const docHeight = document.body.scrollHeight;
+        const winHeight = window.innerHeight;
+        const scrollBottom = docHeight - (scrollTop + winHeight); // Distance from bottom
+
+        const maxScrollDistance = (maxHeightPercentMobile / 100) * docHeight; // max height in px
         let heightPx = maxScrollDistance - scrollBottom;
 
         heightPx = Math.max(0, Math.min(maxScrollDistance, heightPx)); // clamp between 0 and max
@@ -23,7 +39,8 @@ const Contact = () => {
         <div id="contact" className={"contact"}>
             <h1>Let's Connect</h1>
             <iframe src="https://surveyheart.com/form/67f67aa7fa6fac248afa1e64">Loading...</iframe>
-            <div id={"hider"} className={"hider"}/>
+            <div id={"pc-hider"} className={"hider"}/>
+            <div id={"mobile-hider"} className={"hider"}/>
         </div>
     )
 }
